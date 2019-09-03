c919b5d4361b69e0dc03e2c3f10a971fe75ba257
Clark Rushing <crushing05@gmail.com>
2019-09-03 10:58:45 -0600

    Fix paths so tests work

--- testthat/test-datascript.R
+++ testthat/test-datascript.R
@@ -3,7 +3,7 @@ context("checks that datascript is working")
 
 test_that("printed value is correct", {
   
-  expect_output(str(source("./datascript.R")), 
+  expect_output(str(source("../datascript.R")), 
                    "Updating feeder data")
 
 })
